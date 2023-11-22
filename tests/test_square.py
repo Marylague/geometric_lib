f5b9d35c9be44713a1e5963a5a39a3fce3405b23
Marylague <sytinamaria1@gmail.com>
2023-11-22 12:56:31 +0300

    update tests

--- tests/test_square.py
+++ tests/test_square.py
@@ -1,20 +1,35 @@
 import unittest
 
+from square import area
+from square import perimeter
+
 class SquareTestCase(unittest.TestCase) :
     
-    def test_zero_mul(self):
+    def test_zero_area(self):
         
         res = area(0)
         self.assertEqual(res, 0)
     
-    def test_1(self):
+    def test_area1(self):
         
         res = area(5)
         self.assertEqual(res, 25)
 
-    def test_2(self):
+    def test_area2(self):
         
         res = area(70)
         self.assertEqual(res, 4900)
+
+    def test_zero_perimeter(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_perimeter1(self):
+        res = perimeter(6)
+        self.assertEqual(res, 24)
+
+    def test_perimeter2(self):
+        res = perimeter(8)
+        self.assertEqual(res, 32)
 
 
--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -1,20 +1,31 @@
 import unittest
 
+from triangle import area
+from triangle import perimeter
+
 class TriangleTestCase(unittest.TestCase) :
     
-    def test_zero_mul(self):
+    def test_zero_area(self):
         
         res = area(0, 5)
         self.assertEqual(res, 0)
     
-    def test_1(self):
+    def test_area1(self):
         
         res = area(5, 2)
         self.assertEqual(res, 5)
 
-    def test_2(self):
+    def test_area2(self):
         
         res = area(70, 4)
         self.assertEqual(res, 140)
+
+    def test_perimeter1(self):
+        res = perimeter(4, 5, 8)
+        self.assertEqual(res, 17)
+
+    def test_perimeter2(self):
+        res = perimeter(59, 62, 6)
+        self.assertEqual(res, 127)
 
 
